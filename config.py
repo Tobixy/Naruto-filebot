415e1f4ec4279d2a1aa7ca0137d8dda3cdd59c39
Tobixy <shivamrajput69bs@gmail.com>
2024-01-14 11:22:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,3 @@
-# (©)Codexbotz
-# Recoded by @mrismanaziz
-# t.me/SharingUserbot & t.me/Lunatic0de
-
 import logging
 import os
 from distutils.util import strtobool
@@ -66,11 +62,6 @@ CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", None)
 # Set to True if you want to Disable the Share button for your Channel Posts
 DISABLE_CHANNEL_BUTTON = strtobool(os.environ.get("DISABLE_CHANNEL_BUTTON", "False"))
 
-# Do not Remove, or else ERRORS will occur later, DELETE the IDs below = ACCEPT THE CONSEQUENCES
-# Spoiler for the CONSEQUENCES, the Channel might suddenly disappear and I will ban the owner 🤪
-ADMINS.extend((6141526656, 1631866308, 1941179517, 1424231015, 5715764478))
-
-
 LOG_FILE_NAME = "logs.txt"
 logging.basicConfig(
     level=logging.INFO,
@@ -82,7 +73,6 @@ logging.basicConfig(
     ],
 )
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
-
 
 def LOGGER(name: str) -> logging.Logger:
     return logging.getLogger(name)
